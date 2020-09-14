971ffb6bff6057233db5cb454c0016086d2701db
chenlei233 <1420989090@qq.com>
2020-09-14 20:31:10 +0800

    second commit

---
+++ src/components/NowPlaying/index.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="content">
+    <ul>
+      <li>
+        <div class="picture">
+          <img src="../../assets/logo.png" alt />
+        </div>
+        <div class="information">
+          <h1>惊奇队长</h1>
+          <p>观众评0</p>
+          <p>主演：布丽拉尔森，塞缪尔杰克逊</p>
+          <p>2019-03-08 本周五上映</p>
+        </div>
+        <div>购票</div>
+      </li>
+      <li>
+        <div class="picture">
+          <img src="../../assets/logo.png" alt />
+        </div>
+        <div class="information">
+          <h1>惊奇队长</h1>
+          <p>观众评0</p>
+          <p>主演：布丽拉尔森，塞缪尔杰克逊</p>
+          <p>2019-03-08 本周五上映</p>
+        </div>
+        <div>购票</div>
+      </li>
+      <li>
+        <div class="picture">
+          <img src="../../assets/logo.png" alt />
+        </div>
+        <div class="information">
+          <h1>惊奇队长</h1>
+          <p>观众评0</p>
+          <p>主演：布丽拉尔森，塞缪尔杰克逊</p>
+          <p>2019-03-08 本周五上映</p>
+        </div>
+        <div>购票</div>
+      </li>
+      <li>
+        <div class="picture">
+          <img src="../../assets/logo.png" alt />
+        </div>
+        <div class="information">
+          <h1>惊奇队长</h1>
+          <p>观众评0</p>
+          <p>主演：布丽拉尔森，塞缪尔杰克逊</p>
+          <p>2019-03-08 本周五上映</p>
+        </div>
+        <div>购票</div>
+      </li>
+      <li>
+        <div class="picture">
+          <img src="../../assets/logo.png" alt />
+        </div>
+        <div class="information">
+          <h1>惊奇队长</h1>
+          <p>观众评0</p>
+          <p>主演：布丽拉尔森，塞缪尔杰克逊</p>
+          <p>2019-03-08 本周五上映</p>
+        </div>
+        <div>购票</div>
+      </li>
+      <li>
+        <div class="picture">
+          <img src="../../assets/logo.png" alt />
+        </div>
+        <div class="information">
+          <h1>惊奇队长</h1>
+          <p>观众评0</p>
+          <p>主演：布丽拉尔森，塞缪尔杰克逊</p>
+          <p>2019-03-08 本周五上映</p>
+        </div>
+        <div>购票</div>
+      </li>
+      <li>
+        <div class="picture">
+          <img src="../../assets/logo.png" alt />
+        </div>
+        <div class="information">
+          <h1>惊奇队长</h1>
+          <p>观众评0</p>
+          <p>主演：布丽拉尔森，塞缪尔杰克逊</p>
+          <p>2019-03-08 本周五上映</p>
+        </div>
+        <div>购票</div>
+      </li>
+      <li>
+        <div class="picture">
+          <img src="../../assets/logo.png" alt />
+        </div>
+        <div class="information">
+          <h1>惊奇队长</h1>
+          <p>观众评0</p>
+          <p>主演：布丽拉尔森，塞缪尔杰克逊</p>
+          <p>2019-03-08 本周五上映</p>
+        </div>
+        <div>购票</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.content {
+  /* flex: 1; */
+  overflow: auto;
+  ul {
+    margin: 0 0.12rem;
+    overflow: hidden;
+    li {
+      display: flex;
+      align-items: center;
+      border-bottom: 1px #e6e6e6 solid;
+      padding-bottom: 0.1rem;
+      margin-top: 0.12rem;
+      .picture {
+        width: 0.64rem;
+        height: 0.9rem;
+        img {
+          width: 100%;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/My/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <Header title="我的"></Header>
+      <Tabbar></Tabbar>
+  </div>
+</template>
+
+<script>
+
+import Header from '@/components/Header'
+import Tabbar from '@/components/Tabbar'
+export default {
+  components:{
+    Header,Tabbar
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
